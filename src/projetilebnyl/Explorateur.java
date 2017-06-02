8bdcfae4eca9e514f859711facb42451b26ee409
schmuckl <schmuckl@pc-dg-033-08>
2017-06-02 11:45:03 +0200

    Merge origin/master
    Conflicts:
    	src/projetilebnyl/Aventurier.java
    	src/projetilebnyl/VueAventurier.java

--- src/projetilebnyl/Explorateur.java
+++ src/projetilebnyl/Explorateur.java
@@ -1,8 +1,4 @@
 package projetilebnyl;
 
 public class Explorateur extends Aventurier {
-
-    public Explorateur(int colonne, int ligne, Tuile positionCourante) {
-        super(1, colonne, ligne, positionCourante);
-    }
 }
--- src/projetilebnyl/Ingénieur.java
+++ src/projetilebnyl/Ingénieur.java
@@ -2,7 +2,5 @@ package projetilebnyl;
 
 public class Ingénieur extends Aventurier {
 
-    public Ingénieur(int colonne, int ligne, Tuile positionCourante) {
-        super(6, colonne, ligne, positionCourante);
-    }
+    
 }
