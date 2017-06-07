8a541e975ff089b61de4ec840416d1dc86f69365
gervasna <gervasna@pc-dg-039-12>
2017-06-07 15:21:07 +0200

    Merge origin/master
    Conflicts:
    	src/projetilebnyl/Grille.java

--- src/projetilebnyl/Grille.java
+++ src/projetilebnyl/Grille.java
@@ -84,91 +84,41 @@ public class Grille {
                 if(col==0){
                     
                     
-                    if (lig==2){
-                        
-                        
-                        if ( ((tuiles.get(i).getColonne()== col+ 1) && tuiles.get(i).getLigne()==lig )  ||((tuiles.get(i).getLigne()== lig +1) && (tuiles.get(i).getColonne()== col))  ){
-                            Liste.add(tuiles.get(i));
-                        }
-
-                    }else if (lig==3){
-                        
-                        
+                    if (lig==2||lig==3){                       
                         if ( ((tuiles.get(i).getColonne()== col+ 1) && tuiles.get(i).getLigne()==lig )  ||((tuiles.get(i).getLigne()== lig -1) && (tuiles.get(i).getColonne()== col))  ){
-                            Liste.add(tuiles.get(i));
+                            if (tuiles.get(i).getNomCase()!="Vide"){
+                                Liste.add(tuiles.get(i));
+                            }
                         }
                     }
                     
                 }else if(col==5) {     
-                    if (lig==2){
-                        
-                        
-                        if ( ((tuiles.get(i).getColonne()== col- 1) && tuiles.get(i).getLigne()==lig )  ||((tuiles.get(i).getLigne()== lig +1) && (tuiles.get(i).getColonne()== col))  ){
-                            Liste.add(tuiles.get(i));
-                        }
-
-                    }else if (lig==3){
-                        
-                        
+                    if (lig==2||lig==3){                       
                         if ( ((tuiles.get(i).getColonne()== col-1) && tuiles.get(i).getLigne()==lig )  ||((tuiles.get(i).getLigne()== lig -1) && (tuiles.get(i).getColonne()== col))  ){
-                            Liste.add(tuiles.get(i));
-                        }
+                            if (tuiles.get(i).getNomCase()!="Vide"){
+                                Liste.add(tuiles.get(i));
+                            }                        }
                     }
                     
                 }else if(lig==0) { 
-                    if (col==2){
-                        
-                        
-                        if ( ((tuiles.get(i).getColonne()== col+1 ) && tuiles.get(i).getLigne()==lig )  ||((tuiles.get(i).getLigne()== lig+1 ) && (tuiles.get(i).getColonne()== col))  ){
-                            Liste.add(tuiles.get(i));
-                        }
-
-                    }else if (col==3){
-                        
-                        
+                    if (col==2||col==3){
                         if ( ((tuiles.get(i).getColonne()== col-1) && tuiles.get(i).getLigne()==lig )  ||((tuiles.get(i).getLigne()== lig +1) && (tuiles.get(i).getColonne()== col))  ){
-                            Liste.add(tuiles.get(i));
-                        }                   
+                            if (tuiles.get(i).getNomCase()!="Vide"){
+                                Liste.add(tuiles.get(i));
+                            }                        }                   
                     }
                 }else if(lig==5) {   
-                    if (col==2){
-                        
-                        
-                        if ( ((tuiles.get(i).getColonne()== col+ 1) && tuiles.get(i).getLigne()==lig )  ||((tuiles.get(i).getLigne()== lig -1) && (tuiles.get(i).getColonne()== col))  ){
-                            Liste.add(tuiles.get(i));
-                        }
-
-                    }else if (col==3){
-                        
-                        
+                    if (col==2||col==3){                        
                         if ( ((tuiles.get(i).getColonne()== col-1) && tuiles.get(i).getLigne()==lig )  ||((tuiles.get(i).getLigne()== lig -1) && (tuiles.get(i).getColonne()== col))  ){
-                            Liste.add(tuiles.get(i));
-                        }                   
-                    }
-                }else if(col==1 && lig ==1){
-                        if ( ((tuiles.get(i).getColonne()== col+ 1) && tuiles.get(i).getLigne()==lig )  ||((tuiles.get(i).getLigne()== lig +1) && (tuiles.get(i).getColonne()== col))  ){
-                            Liste.add(tuiles.get(i));
-                        }
-
-                }else if(col==1 && lig ==4){
-                        if ( ((tuiles.get(i).getColonne()== col+ 1) && tuiles.get(i).getLigne()==lig )  ||((tuiles.get(i).getLigne()== lig -1) && (tuiles.get(i).getColonne()== col))  ){
-                            Liste.add(tuiles.get(i));
-                        }
-
-                }else if(col== 4 && lig == 1){
-                        if ( ((tuiles.get(i).getColonne()== col- 1) && tuiles.get(i).getLigne()==lig )  ||((tuiles.get(i).getLigne()== lig +1) && (tuiles.get(i).getColonne()== col))  ){
-                            Liste.add(tuiles.get(i));
-                        }
-
-                }else if(col==4 && lig==4){
-                        if ( ((tuiles.get(i).getColonne()== col-1) && tuiles.get(i).getLigne()==lig )  ||((tuiles.get(i).getLigne()== lig -1) && (tuiles.get(i).getColonne()== col))  ){
-                            Liste.add(tuiles.get(i));
-                        }
-                    
+                            if (tuiles.get(i).getNomCase()!="Vide"){
+                                Liste.add(tuiles.get(i));
+                            }                        }                   
+                    }                    
                 }else{  
                     if (((tuiles.get(i).getColonne()== col +1 )&&  tuiles.get(i).getLigne()== lig )|| ((tuiles.get(i).getColonne()== col -1)&&tuiles.get(i).getLigne()== lig) || ((tuiles.get(i).getLigne()== lig +1)&&tuiles.get(i).getColonne()== col) ||((tuiles.get(i).getLigne()== lig -1)&&tuiles.get(i).getColonne()== col ) ){
-                            Liste.add(tuiles.get(i));
-                    }
+                            if (tuiles.get(i).getNomCase()!="Vide"){
+                                Liste.add(tuiles.get(i));
+                            }                    }
                   }      
                 }                
                 
@@ -181,78 +131,42 @@ public class Grille {
                 int lig = positionCourante.getLigne();
                 for(int i=1; i<=tuiles.size();i++) {
                     
-                    
-                if(col==0){
-                    
-                    
-                    if (lig==2||lig==3){
-                        
-                        
-                        if ( ((tuiles.get(i).getColonne()== col+ 1) && tuiles.get(i).getLigne()==lig+1 )  ||((tuiles.get(i).getLigne()== lig -1) && (tuiles.get(i).getColonne()== col+1))  ){
-                            Liste.add(tuiles.get(i));
+                    if(col==0){                  
+                        if (lig==2||lig==3){
+                            if ( ((tuiles.get(i).getColonne()== col+ 1) && tuiles.get(i).getLigne()==lig+1 )  ||((tuiles.get(i).getLigne()== lig -1) && (tuiles.get(i).getColonne()== col+1))  ){
+                                Liste.add(tuiles.get(i));
+                            }
                         }
 
-                    }
-                    
-                }else if(col==5) {     
-                    if (lig==2 || lig==3){
-                        
-                        
-                        if ( ((tuiles.get(i).getColonne()== col- 1) && tuiles.get(i).getLigne()==lig-1 )  ||((tuiles.get(i).getLigne()== lig +1) && (tuiles.get(i).getColonne()== col-1))  ){
-                            Liste.add(tuiles.get(i));
+                    }else if(col==5) {     
+                        if (lig==2 || lig==3){
+                            if ( ((tuiles.get(i).getColonne()== col- 1) && tuiles.get(i).getLigne()==lig-1 )  ||((tuiles.get(i).getLigne()== lig +1) && (tuiles.get(i).getColonne()== col-1))  ){
+                                Liste.add(tuiles.get(i));
+                            }
                         }
-                        
-                    }
-                    
-                }else if(lig==0) { 
-                    if (col==2 || col==3){
-                        
-                        
-                        if ( ((tuiles.get(i).getColonne()== col+1 ) && tuiles.get(i).getLigne()==lig+1 )  ||((tuiles.get(i).getLigne()== lig+1 ) && (tuiles.get(i).getColonne()== col))  ){
-                            Liste.add(tuiles.get(i));
+                    }else if(lig==0) { 
+                        if (col==2 || col==3){
+                            if ( ((tuiles.get(i).getColonne()== col+1 ) && tuiles.get(i).getLigne()==lig+1 )  ||((tuiles.get(i).getLigne()== lig+1 ) && (tuiles.get(i).getColonne()== col))  ){
+                                Liste.add(tuiles.get(i));
+                            }
+                        }else if (col==3){
+                            if ( ((tuiles.get(i).getColonne()== col-1) && tuiles.get(i).getLigne()==lig -1)  ||((tuiles.get(i).getLigne()== lig +1) && (tuiles.get(i).getColonne()== col-1))  ){
+                                Liste.add(tuiles.get(i));
+                            }                   
                         }
-
-                    }else if (col==3){
-                        
-                        
-                        if ( ((tuiles.get(i).getColonne()== col-1) && tuiles.get(i).getLigne()==lig -1)  ||((tuiles.get(i).getLigne()== lig +1) && (tuiles.get(i).getColonne()== col-1))  ){
-                            Liste.add(tuiles.get(i));
-                        }                   
-                    }
-                }else if(lig==5) {   
-                    if (col==2 || col==3){
-                        
-                        
-                        if ( ((tuiles.get(i).getColonne()== col+ 1) && tuiles.get(i).getLigne()==lig-1 )  ||((tuiles.get(i).getLigne()== lig -1) && (tuiles.get(i).getColonne()== col-1))||((tuiles.get(i).getColonne()== col+ 1) && tuiles.get(i).getLigne()==lig-1 )   ){
-                            Liste.add(tuiles.get(i));
+                    }else if(lig==5) {   
+                        if (col==2 || col==3){
+                            if ( ((tuiles.get(i).getColonne()== col+ 1) && tuiles.get(i).getLigne()==lig-1 )  ||((tuiles.get(i).getLigne()== lig -1) && (tuiles.get(i).getColonne()== col-1))||((tuiles.get(i).getColonne()== col+ 1) && tuiles.get(i).getLigne()==lig-1 )   ){
+                                Liste.add(tuiles.get(i));
+                            }
                         }
-                        
-                    }
-                }else if((col==1 && lig ==1)||(col==2 && lig ==1)||(col==1 && lig ==2)){
-                        if ( ((tuiles.get(i).getColonne()== col+ 1) && tuiles.get(i).getLigne()==lig+1 )  ||((tuiles.get(i).getLigne()== lig +1) && (tuiles.get(i).getColonne()== col-1))  ){
-                            Liste.add(tuiles.get(i));
+                    }else{  
+                        if (((tuiles.get(i).getColonne()== col +1 )&&  tuiles.get(i).getLigne()== lig )|| ((tuiles.get(i).getColonne()== col -1)&&tuiles.get(i).getLigne()== lig) || ((tuiles.get(i).getLigne()== lig +1)&&tuiles.get(i).getColonne()== col) ||((tuiles.get(i).getLigne()== lig -1)&&tuiles.get(i).getColonne()== col ) ){
+                            if (tuiles.get(i).getNomCase()!="Vide"){
+                                Liste.add(tuiles.get(i));
+                            }                        
                         }
-
-                }else if((col==1 && lig ==4)||(col==1 && lig ==3)||(col==2 && lig ==4)){
-                        if ( ((tuiles.get(i).getColonne()== col+ 1) && tuiles.get(i).getLigne()==lig+1 )  ||((tuiles.get(i).getLigne()== lig -1) && (tuiles.get(i).getColonne()== col-1))||((tuiles.get(i).getColonne()== col+ 1) && tuiles.get(i).getLigne()==lig-1 )   ){
-                            Liste.add(tuiles.get(i));
-                        }
-
-                }else if((col== 4 && lig == 1)||(col==3 && lig ==1)||(col==2 && lig ==4)){
-                       if ( ((tuiles.get(i).getColonne()== col- 1) && tuiles.get(i).getLigne()==lig-1 )  ||((tuiles.get(i).getColonne()== col- 1) && tuiles.get(i).getLigne()==lig+1 )  ||((tuiles.get(i).getLigne()== lig +1) && (tuiles.get(i).getColonne()== col+1))  ){
-                            Liste.add(tuiles.get(i));
-                        }
-
-                }else if((col==4 && lig==4)||(col==4 && lig ==3)||(col==3 && lig ==4)){
-                        if ( ((tuiles.get(i).getColonne()== col-1) && tuiles.get(i).getLigne()==lig-1 )  ||((tuiles.get(i).getColonne()== col-1) && tuiles.get(i).getLigne()==lig+1 )  ||((tuiles.get(i).getLigne()== lig -1) && (tuiles.get(i).getColonne()== col+1))  ){
-                            Liste.add(tuiles.get(i));
-                        }
-                    
-                }else{  
-                    if (((tuiles.get(i).getColonne()== col +1 )&&  tuiles.get(i).getLigne()== lig )|| ((tuiles.get(i).getColonne()== col -1)&&tuiles.get(i).getLigne()== lig) || ((tuiles.get(i).getLigne()== lig +1)&&tuiles.get(i).getColonne()== col) ||((tuiles.get(i).getLigne()== lig -1)&&tuiles.get(i).getColonne()== col ) ){
-                            Liste.add(tuiles.get(i));
-                    }
-                  }      
+                    }      
                 }                
                 
                 return Liste;
@@ -262,7 +176,7 @@ public class Grille {
             return tuiles;
         }
         
-        public ArrayList<Tuile> getTuile_seche_innonde(){
+        public ArrayList<Tuile> getTuilesPossibles(){
             ArrayList<Tuile> liste = new ArrayList();
             for(Tuile t : tuiles) {
                 if(t.getStatut()==ASSECHEE || t.getStatut()==INONDEE){
@@ -283,7 +197,7 @@ public class Grille {
         
         //uniquement pour test grille
              
-    public void afficheGrille(){
+        public void afficheGrille(){
         for (int i=0;i<(this.getGrille().size());++i){
                this.getGrille().get(i).afficheTuile();System.out.println(" : "+i);
                
